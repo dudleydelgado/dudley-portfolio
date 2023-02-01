474417181027342edf09ebc69df6e9e96a28d46a
dudley delgado <arturobonilla0412@gmail.com>
2023-02-01 23:18:35 +0100

    add new icons, projects, hover states, form submissions, improve mobile menu and fixed visual errors

--- main.js
+++ main.js
@@ -1,6 +1,12 @@
 const menuHamContainer = document.querySelector('.menuContainer');
-const menuHamIcon = document.querySelector('.menuContainer__icon');
+const menuBars = document.querySelector('.menu-bars');
+let lineBarsOne = document.querySelector(".line1__bars-menu");
+let lineBarsTwo = document.querySelector(".line2__bars-menu");
+let lineBarsThree = document.querySelector(".line3__bars-menu");
 
-menuHamIcon.addEventListener('click', () => {
+menuBars.addEventListener("click", () => {
+    lineBarsOne.classList.toggle("activeline1__bars-menu");
+    lineBarsTwo.classList.toggle("activeline2__bars-menu");
+    lineBarsThree.classList.toggle("activeline3__bars-menu");
     menuHamContainer.classList.toggle('inactive');
-})
+});
